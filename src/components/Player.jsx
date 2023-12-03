d92bb993e6098178f6ac70aae4aad0b1a2e48738
Muhammmad Bilal <bilaltheofficial@gmail.com>
2023-12-03 19:31:17 +0500

    add playername state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
   const [playerName, setPlayerName] = useState(initialName)
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     // (editing) => !editing is best practice to change state if value is depend on previous state.
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
