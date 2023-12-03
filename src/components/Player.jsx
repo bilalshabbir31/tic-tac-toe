bebd9b92f70dd1236459c26106c8725c04de430a
Muhammmad Bilal <bilaltheofficial@gmail.com>
2023-12-03 10:44:46 +0500

    add playerName state save name change in inputfield

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName)
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -8,17 +9,21 @@ const Player = ({ name, symbol }) => {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
   // let btnCaption = 'Edit';
   if (isEditing) {
-    playerName = <input type="text" required value={name}/>
+    editablePlayerName = (<input type="text" required value={playerName} onChange={handleChange}/>);
     // btnCaption="Save"
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
