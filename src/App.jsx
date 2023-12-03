d0b2d7593ec7d25bcfd3d3d7b117b7c1fa51189d
Muhammmad Bilal <bilaltheofficial@gmail.com>
2023-12-03 16:50:13 +0500

    store game turn in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import { useState } from "react"
+import Log from "./components/Log";
 
 function App() {
 
+  const [gameTurn, setGameTurn] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurn((prevTurns) => {
+
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
+      
+      return updateTurns
+    });
   }
 
   return (
@@ -17,8 +30,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurn}/>
       </div>
+      <Log/>
     </main>
   )
 }
